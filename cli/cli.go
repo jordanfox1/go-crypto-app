a917172347469739e22123293622d092e2bdad5a
Jordan Fox <jordanfox9415@gmail.com>
2023-10-04 18:23:13 +1000

    finished decryption cli interface

--- cli/cli.go
+++ cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"bufio"
+	"crypto_cli/decrypt"
 	"crypto_cli/encrypt"
 	"os"
 
@@ -17,10 +18,18 @@ func StartEncrypt() {
 	// Create a new scanner to read from stdin
 	scanner := bufio.NewScanner(os.Stdin)
 
+	option, err := selectOption()
+	if err != nil {
+		e.Println("Invalid option", option)
+		os.Exit(1)
+	}
+	if option == "dec" {
+		startDecrypt()
+	}
+
 	// Prompt the user for input
 	prompt.Println("Enter the name of the file for encryption below and press Enter: ")
 
-	// Read user input
 	if scanner.Scan() {
 		userInput := scanner.Text()
 		prompt.Println("You entered: ", userInput)
@@ -81,4 +90,74 @@ func checkIfFileExists(userInput string) error {
 		return err
 	}
 	return nil
+}
+
+func selectOption() (string, error) {
+	prompt.Println("Type enc for encryption or dec for decryption and press enter")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	err := scanner.Err()
+	if err != nil {
+		return "", err
+	}
+	userInput := scanner.Text()
+	if userInput == "enc" || userInput == "dec" {
+		return userInput, nil
+	}
+	e.Println("Invalid input", userInput)
+	os.Exit(1)
+	return "", nil
+}
+
+func startDecrypt() {
+	scanner := bufio.NewScanner(os.Stdin)
+	// Prompt the user for input
+	prompt.Println("Enter the name of the file for decryption below and press Enter: ")
+
+	if scanner.Scan() {
+		userInput := scanner.Text()
+		prompt.Println("You entered: ", userInput)
+		process.Println("Atempting to decrypt file in current directory named:", userInput)
+
+		err := checkIfFileExists(userInput)
+		if err != nil {
+			os.Exit(1)
+		}
+
+		bar := progressbar.Default(100)
+		bar.Add(50)
+
+		nonceFileName := "nonce.txt"
+		keyFileName := "enc_key.txt"
+		decDataFileName := "decrypted.txt"
+
+		key, err := os.ReadFile(keyFileName)
+		if err != nil {
+			e.Println("Error reading encryption key from file named enc_txt", err)
+			os.Exit(1)
+		}
+		bar.Add(10)
+		bar.Add(10)
+
+		nonce, err := os.ReadFile(nonceFileName)
+		if err != nil {
+			e.Println("Error reading nonce", err)
+			os.Exit(1)
+		}
+		bar.Add(10)
+
+		err = decrypt.DecryptCipherText(userInput, decDataFileName, key, nonce)
+		if err != nil {
+			e.Println("Error performing encryption", err)
+			os.Exit(1)
+		}
+		bar.Add(20)
+		prompt.Println("Decryption Successful!")
+		process.Printf("The following files have been created in the current directory - decrypted data: %v", decDataFileName)
+		os.Exit(0)
+
+	} else if err := scanner.Err(); err != nil {
+		e.Println("Error reading input:", err)
+	}
 }
